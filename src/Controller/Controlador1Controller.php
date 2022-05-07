865d079e361c0800deee2fc30eb3407006e33427
mark-blazquez <blazquezmark97@gmail.com>
2022-05-07 23:53:39 +0200

    controlador que muestra procesos de terminal linux

---
+++ src/Controller/Controlador1Controller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/*
+class Controlador1Controller extends AbstractController
+{
+    #[Route('/controlador1', name: 'app_controlador1')]
+    public function index(): Response
+    {
+        return $this->render('controlador1/index.html.twig', [
+            'controller_name' => 'Controlador1Controller',
+        ]);
+    }
+}*/
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+$process = new Process(['pwd']);
+//$process = new Process(['ls -la']);
+
+$process->run();
+
+// executes after the command finishes
+if (!$process->isSuccessful()) {
+    throw new ProcessFailedException($process);
+}
+
+echo $process->getOutput();
