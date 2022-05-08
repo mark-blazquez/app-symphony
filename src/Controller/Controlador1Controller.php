8991fb748326bf0d5217822865c34762ef6eb286
mark-blazquez <blazquezmark97@gmail.com>
2022-05-08 22:23:13 +0200

    añadido formato al obtener informacion de terminal

--- src/Controller/Controlador1Controller.php
+++ src/Controller/Controlador1Controller.php
@@ -1,31 +1,64 @@
 <?php
+/*use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+$process = new Process(['/usr/bin/kubectl', 'get', 'pod']);
+
+		$process->run();
+
+		// executes after the command finishes
+		if (!$process->isSuccessful()) {
+			throw new ProcessFailedException($process);
+		}
+
+		$kubernetes = $process->getOutput();
+		$kubernetes = str_replace(PHP_EOL, '<br>', $kubernetes);
+		$kubernetes = str_replace(" "," ", $kubernetes);
+
+		$array = explode(" ", $kubernetes);
+		foreach($array as $nombre){
+			echo $nombre ;
+			echo "-" ;
+
+		};
+*/
 
 namespace App\Controller;
-/*
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class Controlador1Controller extends AbstractController
 {
     #[Route('/controlador1', name: 'app_controlador1')]
     public function index(): Response
     {
-        return $this->render('controlador1/index.html.twig', [
+		$process = new Process(['/usr/bin/kubectl', 'get', 'pod']);
+
+		$process->run();
+
+		// executes after the command finishes
+		if (!$process->isSuccessful()) {
+			throw new ProcessFailedException($process);
+		}
+
+		$kubernetes = $process->getOutput();
+		$kubernetes = str_replace(PHP_EOL, '<br>', $kubernetes);
+		$kubernetes = str_replace(" ","-", $kubernetes);
+
+		$array = explode(" ", $kubernetes);
+		foreach($array as $nombre){
+			echo $nombre;
+		};
+
+
+
+       return $this->render('controlador1/index.html.twig', [
             'controller_name' => 'Controlador1Controller',
+			'info_app' => $kubernetes ,
         ]);
     }
-}*/
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
-
-$process = new Process(['/usr/bin/kubectl', 'get','all']);
-
-$process->run();
-
-// executes after the command finishes
-if (!$process->isSuccessful()) {
-    throw new ProcessFailedException($process);
 }
-
-echo $process->getOutput();
