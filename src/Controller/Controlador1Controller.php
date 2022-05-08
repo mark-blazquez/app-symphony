30a733ebe57e32cf33a8b62ff2fb5d3e68e9c0b6
mark-blazquez <blazquezmark97@gmail.com>
2022-05-08 18:44:58 +0200

    funcionando comando kubectl get all controlador 1

--- src/Controller/Controlador1Controller.php
+++ src/Controller/Controlador1Controller.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace App\Controller;
-
+/*
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-/*
+
 class Controlador1Controller extends AbstractController
 {
     #[Route('/controlador1', name: 'app_controlador1')]
@@ -19,8 +19,7 @@ class Controlador1Controller extends AbstractController
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
-$process = new Process(['pwd']);
-//$process = new Process(['ls -la']);
+$process = new Process(['/usr/bin/kubectl', 'get','all']);
 
 $process->run();
 
