30d05fef7d93011df5450ff90f8c7fe002874062
mark-blazquez <blazquezmark97@gmail.com>
2022-05-08 23:11:33 +0200

    acabado el apartado estetico para la recoleccion de datos de terminal

--- src/Controller/Controlador1Controller.php
+++ src/Controller/Controlador1Controller.php
@@ -47,12 +47,36 @@ class Controlador1Controller extends AbstractController
 
 		$kubernetes = $process->getOutput();
 		$kubernetes = str_replace(PHP_EOL, '<br>', $kubernetes);
-		$kubernetes = str_replace(" ","-", $kubernetes);
+		$kubernetes = str_replace(" ","_", $kubernetes);
+		$kubernetes = str_replace("______________________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("______________________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("_____________________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("____________________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("___________________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("__________________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("_________________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("________________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("_______________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("______________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("_____________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("____________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("___________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("__________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("_________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("________","</td><td>", $kubernetes);
+		$kubernetes = str_replace("_______","</td><td>", $kubernetes);
+		$kubernetes = str_replace("______","</td><td>", $kubernetes);
+		$kubernetes = str_replace("_____","</td><td>", $kubernetes);
+		$kubernetes = str_replace("____","</td><td>", $kubernetes);
+		$kubernetes = str_replace("___","</td><td>", $kubernetes);
+		$kubernetes = str_replace("__","</td><td>", $kubernetes);
+		//$kubernetes = str_replace("_","</td><td>", $kubernetes);
+		$kubernetes = str_replace('<br>', '<tr><td>', $kubernetes);
 
-		$array = explode(" ", $kubernetes);
-		foreach($array as $nombre){
-			echo $nombre;
-		};
+
+
+
+
 
 
 
