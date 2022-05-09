9c872e3f545df8ccb949cbd5e2cdf58b3ebd7616
mark-blazquez <blazquezmark97@gmail.com>
2022-05-09 23:16:52 +0200

    añadido estilo de forma correcta

--- src/Controller/Controlador1Controller.php
+++ src/Controller/Controlador1Controller.php
@@ -1,26 +1,4 @@
 <?php
-/*use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
-$process = new Process(['/usr/bin/kubectl', 'get', 'pod']);
-
-		$process->run();
-
-		// executes after the command finishes
-		if (!$process->isSuccessful()) {
-			throw new ProcessFailedException($process);
-		}
-
-		$kubernetes = $process->getOutput();
-		$kubernetes = str_replace(PHP_EOL, '<br>', $kubernetes);
-		$kubernetes = str_replace(" "," ", $kubernetes);
-
-		$array = explode(" ", $kubernetes);
-		foreach($array as $nombre){
-			echo $nombre ;
-			echo "-" ;
-
-		};
-*/
 
 namespace App\Controller;
 
